a6c9a523276f4b79c50729d4f1a825cea588c7a7
max <haufemax@gmail.com>
2024-06-28 00:13:09 +0200

    feat: added saving of dict, added serializing of frozensets instead of using str as dict keys

--- src/make_distribution_figures.py
+++ src/make_distribution_figures.py
@@ -1,4 +1,5 @@
 import os
+import z3
 import matplotlib.pyplot as plt
 import pickle
 import random
@@ -12,13 +13,26 @@ from samplers.distance_based import DistanceBasedSampler
 
 fm_path = '../dimacs/small/final'
 ground_truth_path = '../out/final'
+# ground_truth_path = '../out/testing_dimacs'
 
-num_times = 1000  # sample 100 times TODO: this needs to be larger
+# this is where the saved dicts go
+out_path = '../out/distribution_figures'
+
+num_times = 400  # sample 100 times TODO: this needs to be larger
+# num_times = 1000  # sample 100 times TODO: this needs to be larger
 
 
 def sample_ground_truth(ground_truth: set, n: int) -> set:
     # ohne zurücklegen ziehen
     return set(random.sample(ground_truth, n))
+
+
+def serialize_model(model: z3.ModelRef) -> frozenset:
+    serialized = set()
+    for d in model.decls():
+        value = model[d]
+        serialized.add((str(d), bool(value)))
+    return frozenset(serialized)
 
 
 def perform_sampling(fm_path, base_name, gt, n_samples):
@@ -29,23 +43,32 @@ def perform_sampling(fm_path, base_name, gt, n_samples):
 
     s_dp = dps.sample(n_samples)
     s_db = dbs.sample(n_samples)
-    # convert Model Refs to strings
-    s_dp = {str(c) for c in s_dp}
-    s_db = {str(c) for c in s_db}
 
+    # serialize the models becuase python cant save ModelRefs (ctype pointers) to disk
+    ss_dp = {serialize_model(c) for c in s_dp}
+    ss_db = {serialize_model(c) for c in s_db}
     s_gt = sample_ground_truth(gt, n_samples)
 
-    return s_dp, s_db, s_gt
+    return ss_dp, ss_db, s_gt
 
 
 if __name__ == '__main__':
+    # print the directory
+    print(os.listdir(ground_truth_path))
     for ground_truth in tqdm(os.listdir(ground_truth_path), desc="Files"):
+        print(f'Processing {ground_truth}')
+        if ground_truth == 'berkeleydb.dimacs.pickle' or ground_truth == 'blasted_case21.cnf.pickle':
+            # skip
+            print(f'skipping {ground_truth}')
+            continue
+            
         with open(f'{ground_truth_path}/{ground_truth}', 'rb') as f:
             gt = pickle.load(f)
         # remove the .pickle extension
         base_name = ground_truth.replace('.pickle', '')
 
         n_samples = len(gt) // 100  # sample 1% of the ground truth
+        # n_samples = 4  # sample 1% of the ground truth
         print(f'Sampling {n_samples} samples {num_times} times')
         d = {'DP': defaultdict(int), 'DB': defaultdict(int), 'GT': defaultdict(int)}
 
@@ -67,16 +90,14 @@ if __name__ == '__main__':
                 for sample in s_gt:
                     d['GT'][sample] += 1
 
-        # Print the counts
-        # for method, counts in d.items():
-        #     print(f'Counts for {method}:')
-        #     for sample, count in counts.items():
-        #         if count > 1:
-        #             print(f'{method} -> {count}')
 
         # Prepare data for plotting
         max_count = 0
         multiplicities = {'DP': defaultdict(int), 'DB': defaultdict(int), 'GT': defaultdict(int)}
+
+        # save multiplicities
+        with open(f'{out_path}/{base_name}_{num_times}_{n_samples}.pickle', 'wb') as f:
+            pickle.dump(d, f)
 
         for method, counts in d.items():
             for sample, count in counts.items():
@@ -96,3 +117,4 @@ if __name__ == '__main__':
         plt.title(f'Dist for {base_name}')
         plt.legend()
         plt.show()
+        # plt.savefig(f'{out_path}/{base_name}_{num_times}_{n_samples}.png')
